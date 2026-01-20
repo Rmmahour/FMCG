2fc221693c266609a9c07c42b0ce4572c50224d4
Ram <rmmahour@gmail.com>
2026-01-20 15:39:09 +0530

    tags added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { GoogleTagManager, GoogleAnalytics } from '@next/third-parties/google'
 import { Inter } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
+import Script from "next/script" 
 import { CookieConsent } from "@/components/cookie-consent"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -32,6 +33,30 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="favicon.webp" />
+      
+        {/* Meta Pixel Code */}
+<Script
+  id="facebook-pixel"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '733484639407288');
+      fbq('track', 'PageView');
+    `,
+  }}
+/>
+<noscript><img height="1" width="1" style={{display:'none'}}
+src="https://www.facebook.com/tr?id=733484639407288&ev=PageView&noscript=1"
+/></noscript>
+ {/* End Meta Pixel Code  */}
       </head>
       <body
         className={`${inter.className} antialiased min-h-screen flex flex-col bg-gradient-to-br from-[#e6f4ff] via-white to-[#fdf7ec]`}
